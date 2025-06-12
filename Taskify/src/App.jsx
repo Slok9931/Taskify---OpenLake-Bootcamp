11baabc0bd9f9e628585a315757bc71b092ac4fb
HerShey-sys <harshithavankadara52@gmail.com>
2025-06-12 23:26:24 +0530

    issue:3 done

--- Taskify/src/App.jsx
+++ Taskify/src/App.jsx
@@ -1,7 +1,25 @@
+
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Login from "./Login"
+import Todo from "./Todo"
+import Signup from "./Signup";
+import Note from "./Note";
+
 function App() {
   return (
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/todo" element={<Todo />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/note" element={<Note />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
