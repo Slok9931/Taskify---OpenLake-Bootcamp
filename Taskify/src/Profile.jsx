1921a3ef3db06979d4187eb6a724ddc62531fa77
RavikantiAkshay <ravikantia@iitbhilai.ac.in>
2025-06-14 11:40:19 +0530

    issue #10 fetched from local storage, sent and displayed

--- Taskify/src/Profile.jsx
+++ Taskify/src/Profile.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import ProfileComponent from './components/ProfileComponent';
 
-const Profile = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+function Profile() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const storedUsers = localStorage.getItem("users");
+    if (storedUsers) {
+      const usersArray = JSON.parse(storedUsers);
+      if (usersArray.length > 0) {
+        const latestUser = usersArray[usersArray.length - 1];
+        setUserData(latestUser);
+      }
+    }
+  }, []);
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
+
+  return <ProfileComponent userData={userData} />;
 }
 
-export default Profile
+export default Profile;
